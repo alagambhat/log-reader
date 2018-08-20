7d1feef424773074d5213ff808045b7e5a09cf88
Pramod <pramod.alagambhat@gmail.com>
2018-08-20 21:15:13 +0100

    Initial commit
    Signed-off-by: Pramod <pramod.alagambhat@gmail.com>

---
+++ src/main/java/com/log/reader/JsonLogReaderApplication.java
@@ -0,0 +1,12 @@
+package com.log.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonLogReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonLogReaderApplication.class, args);
+	}
+}
