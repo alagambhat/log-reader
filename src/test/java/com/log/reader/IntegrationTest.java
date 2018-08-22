4c5654aefeb8b32bcbd91d71504270523453eee9
earpram <a.r.pramod@ericsson.com>
2018-08-22 10:50:51 +0100

    Added end-end test

--- src/test/java/com/log/reader/IntegrationTest.java
+++ src/test/java/com/log/reader/IntegrationTest.java
@@ -1,4 +1,4 @@
-package com.log.reader.controller;
+package com.log.reader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -13,20 +13,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.log.reader.db.model.Event;
-import com.log.reader.log.model.LogEvent;
-import com.log.reader.service.DbService;
+import com.log.reader.service.FileReader;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
 		"job.autorun.enabled=false" })
-public class ReaderControllerTest {
+public class IntegrationTest {
 
 	@LocalServerPort
 	int port;
 
 	@Autowired
-	private DbService dbService;
+	private FileReader fileReader;
 
 	@Autowired
 	private TestRestTemplate restTemplate;
@@ -39,19 +37,16 @@ public class ReaderControllerTest {
 	}
 
 	/**
-	 * Verifies that the data is stored in DB and is read using the REST end point.
+	 * Verifies that the expected data (with threshold > 4) is stored in DB and is
+	 * read using the REST end point.
 	 */
 	@Test
 	public void testDataStoredinDb() throws Exception {
-		save();
+		fileReader.read("src/test/resources/sample.log");
 		String url = "http://localhost:" + port + "/read/all";
-		assertThat(this.restTemplate.getForObject(url, String.class)).contains(
-				"\"id\":\"12345\",\"eventDuration\":8,\"type\":\"dummyType\",\"host\":\"dummyHost\",\"alert\":true}");
-	}
-
-	private void save() {
-		LogEvent entry = new LogEvent("12345", "FINISHED", 7866542314L, "dummyType", "dummyHost");
-		dbService.save(entry, 8L);
+		final String expectedEntry1 = "\"id\":\"scsmbstgra\",\"eventDuration\":5,\"type\":\"APPLICATION_LOG\",\"host\":\"12345\",\"alert\":true";
+		final String expectedEntry2 = "\"id\":\"scsmbstgrc\",\"eventDuration\":8,\"type\":null,\"host\":null,\"alert\":true";
+		assertThat(this.restTemplate.getForObject(url, String.class)).contains(expectedEntry1, expectedEntry2);
 	}
 
 }
