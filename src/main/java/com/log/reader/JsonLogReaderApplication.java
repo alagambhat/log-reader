49af9aeeff55a613e7ac5f93d6d49491558a07d1
earpram <a.r.pramod@ericsson.com>
2018-08-22 12:46:25 +0100

    Removed EnableAsync as Events are now used.

--- src/main/java/com/log/reader/JsonLogReaderApplication.java
+++ src/main/java/com/log/reader/JsonLogReaderApplication.java
@@ -1,29 +1,13 @@
 package com.log.reader;
 
-import java.util.concurrent.Executor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
-@EnableAsync //Used to enable async operation
 public class JsonLogReaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonLogReaderApplication.class, args);
 	}
-	
-    @Bean
-    public Executor asyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(5);
-        executor.setQueueCapacity(500);
-        executor.initialize();
-        return executor;
-    }
 
 }
